53dc9213939ff7edfbc07a23e5ca46b1c7ec606d
Jose Tone <developer.tony17@gmail.com>
2023-07-07 22:04:02 +0100

    Initial Commit

---
+++ Quotes+/Quotes_App.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_App.swift
+//  Quotes+
+//
+//  Created by José Tony on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
